354f262ab0aa8553c90c8e025a334cc0d88ac9c1
aakash-791 <122847899+aakash-791@users.noreply.github.com>
2023-06-10 00:36:05 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (97.80%) - LeetHub

--- 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.cpp
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.cpp
@@ -1,12 +1,26 @@
 class Solution {
 public:
+    int arrayMax(vector<int>&arr){
+        int maxi = INT_MIN;
+        for(int i =0;i < arr.size();i++)
+            maxi = max(arr[i],maxi);
+     return maxi;
+    }
+    int arrayMin(vector<int>&arr){
+        int mini = INT_MAX;
+        for(int i =0;i < arr.size();i++)
+            mini = min(arr[i],mini);
+     return mini;
+    }
     int countElements(vector<int>& nums) {
         int n = nums.size();
-        sort(nums.begin(),nums.end());
-        int cnt = 0;
-        for(int i=1;i<n-1;i++)
-         if(nums[i] != nums[0] && nums[i] != nums[n-1])
-            cnt++;
+        int maxi = arrayMax(nums);
+        int mini = arrayMin(nums);
+        int cnt =0;
+        for(int i =0;i<=n-1;i++){
+            if(nums[i] != mini && nums[i] != maxi)
+                cnt++;
+        }
         return cnt;
     }
 };
