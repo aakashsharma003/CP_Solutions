3ad9540f9e4e69a5ea43360d389d8ea37966d66f
demaxxer-7911 <122847899+demaxxer-7911@users.noreply.github.com>
2023-08-18 21:55:08 +0530

    Added solution - LeetHub

--- Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
@@ -9,26 +9,31 @@ using namespace std;
 
 class Solution{
 public:
-   int solve(int indx,int w,int *val,int *wt,vector<vector<int>>&dp){
-       if(indx == 0){
-        if(w % wt[0] == 0) return val[0]*(w/wt[0]);
-        else if(wt[0] < w) return val[0];
-        return 0;
-       }
-       if(dp[indx][w] != -1) return dp[indx][w];
-       int notpick = solve(indx-1,w,val,wt,dp);
-       int pick = 0;
-       if(wt[indx] <= w)
-          pick = val[indx] + solve(indx,w-wt[indx],val,wt,dp);
-          
-       return dp[indx][w] = max(pick,notpick);
-   }
-    int knapSack(int n, int w, int val[], int wt[])
+
+    int knapSack(int n, int maxweight, int val[], int wt[])
     {
-       vector<vector<int>>dp(n,vector<int>(w+1,-1));
-        return solve(n-1,w,val,wt,dp);
+       vector<int> prev(maxweight+1,0),cur(maxweight+1,0);
+        for(int w = 1;w<=maxweight;w++){
+            if(w % wt[0] == 0)
+            prev[w] = val[0]*(w / wt[0]);
+            else prev[w] = val[0]*(w/wt[0]);
+        }
+          
+        for(int indx = 1;indx<n;indx++){
+            for(int w = 1;w <= maxweight;w++){
+                
+             int notpick = prev[w];
+             int pick = 0;
+             if(wt[indx] <= w)
+             pick = val[indx] + cur[w-wt[indx]];
+             cur[w] = max(pick,notpick);
+            }
+            prev = cur;
+        }
+        return prev[maxweight];
     }
 };
+
 
 //{ Driver Code Starts.
 
