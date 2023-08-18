5e095240080a16be4176de9f09ce4709d5769488
demaxxer-7911 <122847899+demaxxer-7911@users.noreply.github.com>
2023-08-18 20:55:32 +0530

    Time: 38 ms (27.81%), Space: 18.3 MB (53.83%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    int solve(int indx,vector<int>&coins,int target,vector<vector<int>>&dp){
-         
-        if(indx == 0) return target % coins[0] == 0;
-        
-        
-        if(dp[indx][target] != -1) return dp[indx][target];
-        
-        int notTake = solve(indx-1, coins, target,dp);
-        
-        int take = 0;
-        
-        if(coins[indx] <= target)
-            take = solve(indx, coins,target-coins[indx],dp);
-        
-        return dp[indx][target] = take + notTake;
-    }
     int change(int amount, vector<int>& coins) {
           int n = coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        return solve(n-1,coins,amount,dp);
+          vector<vector<int>>dp(n,vector<int>(amount+1,0));
+        
+        for(int target = 0;target<=amount;target++){
+            dp[0][target] = (target%coins[0] == 0);
+        }
+        
+        for(int indx = 1;indx<n;indx++){
+            for(int target = 0;target <= amount;target++){
+                      
+               int notTake = dp[indx-1][target];
+        
+               int take = 0;
+        
+               if(coins[indx] <= target)
+               take = dp[indx][target-coins[indx]];
+        
+               dp[indx][target] = take + notTake;  
+            }
+        }
+        
+     return dp[n-1][amount];
     }
 };
