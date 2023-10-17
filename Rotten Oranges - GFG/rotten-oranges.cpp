5aa0117dabda1086184b58940dbc6e14f0efd3c2
demaxxer-7911 <122847899+demaxxer-7911@users.noreply.github.com>
2023-10-17 15:09:47 +0530

    Added solution - LeetHub

--- Rotten Oranges - GFG/rotten-oranges.cpp
+++ Rotten Oranges - GFG/rotten-oranges.cpp
@@ -6,29 +6,14 @@ using namespace std;
 class Solution 
 {
     public:
-    //Function to find minimum time required to rot all oranges.
-   
-    //  void bfs(vector<vector<int>>& grid,int row,int col,vector<vector<int>> &vis, int &time){
-         
-    //      int n = grid.size();
-    //      int m = grid[0].size();
-   
-    //      q.push({row,col});
-    //      vis[row][col] = 1;
-    //      bool flag = false;
-         
-         
-    //     return;
-    //  }
     int orangesRotting(vector<vector<int>>& grid) {
         // Code here
          int n = grid.size();
          int m = grid[0].size();
         queue<pair<pair<int,int>,int>>q;
-         int time = 0;
         vector<vector<int>>vis(n,vector<int>(m,0));
-        for(int i =0;i<grid.size();i++){
-            for(int j =0;j < grid[0].size();j++){
+        for(int i =0;i <grid.size();i++){
+            for(int j = 0; j < grid[0].size();j++){
                 if(grid[i][j] == 2){
                     q.push({{i,j},0});
                     vis[i][j] = 2;
@@ -36,40 +21,30 @@ class Solution
             }
         }
         
-        
+        int time = 0;
+        int drow[] = {-1,0,1,0};
+        int dcol[] = {0,1,0,-1};
         while(!q.empty()){
-             int row = q.front().first.first;
-             int col = q.front().first.second;
-             time = q.front().second;
-            //  flag = false;
+             int r = q.front().first.first;
+             int c = q.front().first.second;
+             int t = q.front().second;
              q.pop();
              
-            if(row > 0 && grid[row-1][col] == 1 && !vis[row-1][col]){
-                q.push({{row-1,col},time+1});
-                vis[row-1][col] = 2;
-                grid[row-1][col] = 2;
+             time = max(time,t);
+            for(int i = 0;i< 4;i++){
+                int nrow = r + drow[i];
+                int ncol = c + dcol[i];
                 
-            }
-            if(col > 0 && grid[row][col-1] == 1 && !vis[row][col-1]){
-                q.push({{row,col-1},time+1});
-                vis[row][col-1] = 2;
-                grid[row][col-1] = 2;
-            }
-            if(col+1 < m && grid[row][col+1] == 1 && !vis[row][col+1]){
-                q.push({{row,col+1},time+1});
-                vis[row][col+1] = 2;
-                grid[row][col+1] = 2;
-            }
-            if(row+1 < n && grid[row+1][col] == 1 && !vis[row+1][col]){
-                q.push({{row+1,col},time+1});
-                vis[row+1][col] = 2;
-                grid[row+1][col] = 2;
+                if(0 <= nrow && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == 1 && vis[nrow][ncol] != 2){
+                    q.push({{nrow,ncol},t+1});
+                    vis[nrow][ncol] = 2;
+                }
             }
          }
         
-          for(int i = 0;i < grid.size();i++){
-             for(int j = 0;j < grid[0].size();j++){
-                 if(grid[i][j] == 1){
+          for(int i = 0;i < n;i++){
+             for(int j = 0;j < m;j++){
+                 if(vis[i][j] != 2 && grid[i][j] == 1){
                     return -1;
                  }
              }
